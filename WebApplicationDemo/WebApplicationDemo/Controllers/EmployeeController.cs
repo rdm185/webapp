4bdbb4bf1dd43e33b015aeac1a4868bd396f0dbb
rdm185 <rdm185@gmail.com>
2019-07-13 11:15:21 +0530

    web application

---
+++ WebApplicationDemo/WebApplicationDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationDemo.Models;
+
+namespace WebApplicationDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index(int departmentId)
+        {
+            EmployeeContext employeeContext = new EmployeeContext();
+            // List<Employee> employees = employeeContext.Employees.ToList();
+
+            List<Employee> employees = employeeContext.Employees.Where(emp => emp.DepartmentId == departmentId).ToList();
+            return View(employees);
+        }
+        public ActionResult Details(int id)
+        {
+            EmployeeContext employeeContext = new EmployeeContext();
+            Employee employee = employeeContext.Employees.Single(emp => emp.EmployeeId == id);
+           
+            return View(employee);
+        }
+    }
+}
---
+++ WebApplicationDemo/WebApplicationDemo/Dal/dalEmployees.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using WebApplicationDemo.Models;
+
+namespace WebApplicationDemo.Dal
+{
+    public class dalEmployees
+    {
+        EmployeeContext _db = new EmployeeContext();
+
+
+        public string Save()
+        {
+
+            var parParam = new SqlParameter("Date",System.DateTime.Now);
+
+            _db.Database.SqlQuery<string>("SpName @Date",parParam).ToList();
+
+            return "";
+        }
+    }
+}
